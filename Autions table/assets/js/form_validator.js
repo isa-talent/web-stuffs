17ec79933b94dd2a0ba06e64d087656cd01bd902
isa-talent <88469220+isa-talent@users.noreply.github.com>
2023-03-07 14:42:46 +0000

    Add files via upload

---
+++ Autions table/assets/js/form_validator.js
@@ -0,0 +1,95 @@
+/* Form validation */
+
+function validate() {
+
+  if( document.myForm.Name.value == "" ) {
+    alert( "Please provide your name!" );
+    document.myForm.Name.focus() ;
+    return false;
+  }
+  if( document.myForm.title.value == "" ) {
+    alert( "Please choose title" );
+    document.myForm.title.focus();
+    return false;
+  }
+  if( document.myForm.Country.value == "0" ) {
+    alert( "Please select your country!" );
+    document.myForm.Country.focus();
+    return false;
+  }
+  if( document.myForm.email.value == "" ) {
+    alert( "Please provide your Email!" );
+    document.myForm.email.focus() ;
+    return false;
+  }
+  if( document.myForm.number.value == "" ) {
+    alert( "Please enter phone number" );
+    document.myForm.number.focus();
+    return false;
+  }
+  if( document.myForm.address.value == "" ) {
+    alert( "Please provide Address!" );
+    document.myForm.address.focus();
+    return false;
+  }
+  if( document.myForm.password.value == "" ) {
+    alert( "Please enter password!" );
+    document.myForm.password.focus();
+    return false;
+  }
+  if( document.myForm.password1.value == "" ) {
+    alert( "Please enter password!" );
+    document.myForm.password1.focus();
+    return false;
+  }  
+  if( (document.myForm.password.value) != (document.myForm.password) ) {
+    alert( "Passwords does not match" );
+    document.myForm.password1.focus();
+    return false;
+  }
+
+  alert ("")
+  return( true );
+}
+/* End of Form validation */
+
+/* Request form validation */
+function validater(){
+  if( document.myForm.Name.value == "" ) {
+    alert( "Please provide your name!" );
+    document.myForm.Name.focus() ;
+    return false;
+  }
+  if( document.myForm.title.value == "" ) {
+    alert( "Please choose title" );
+    document.myForm.title.focus();
+    return false;
+  }
+  if( document.myForm.Country.value == "0" ) {
+    alert( "Please select your country!" );
+    document.myForm.Country.focus();
+    return false;
+  }
+  if( document.myForm.email.value == "" ) {
+    alert( "Please provide your Email!" );
+    document.myForm.email.focus() ;
+    return false;
+  }
+  if( document.myForm.number.value == "" ) {
+    alert( "Please enter phone number" );
+    document.myForm.number.focus();
+    return false;
+  }
+  if( document.myForm.address.value == "" ) {
+    alert( "Please provide Address!" );
+    document.myForm.address.focus();
+    return false;
+  }
+  if( document.myForm.bid_amount.value == "" ) {
+    alert( "Please enter amount to bid" );
+    document.myForm.bid_amount.focus();
+    return false;
+  }
+}
+
+/* Request form validation end*/
